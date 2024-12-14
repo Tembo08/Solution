3c22b52bbcb2e3ea1a008f516adfefc4289a240d
Tembo08 <romasaharok775@gmail.com>
2024-12-15 00:26:35 +0300

    Внес изменения 0ч:25 минут

--- src/Solution/Main.java
+++ src/Solution/Main.java
@@ -1,115 +1,65 @@
 package Solution;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        String inputText = sc.nextLine();
-
+        Scanner scanner = new Scanner(System.in);
+        String inputText = scanner.nextLine();
         System.out.println(calc(inputText));
+
     }
 
-
-
+    static char action;
     public static String calc(String input) throws Exception {
-
         String result = "";
-        if (input.contains("+")) {
-            String[] split = input.split("[+]");
+        if (input.contains("/") || input.contains("+") || input.contains("*") || input.contains("-")) {
+            if (input.contains("+")) {
+                action = '+';
+            } else if (input.contains("-")) {
+                action = '-';
+            } else if (input.contains("/")) {
+                action = '/';
+            } else {
+                action = '*';
+            }
+            String[] split = input.split("[+/*-]");
+
             if (split.length > 2) {
                 throw new Exception("формат математической операции не удовлетворяет заданию" +
                         " - два операнда и один оператор (+, -, /, *)");
             }
-            double a = Double.parseDouble(split[0]);
-            double b = Double.parseDouble(split[1]);
-            if (a % 1 != 0) {
+            int a = Integer.parseInt(split[0]);
+            int b = Integer.parseInt(split[1]);
+            if ((a % 1 != 0) || (b % 1 != 0)) {
                 throw new Exception("Калькулятор работает только с целыми числами");
             }
-            if (b % 1 != 0) {
-                throw new Exception("Калькулятор работает только с целыми числами");
-            }
-            if (a < 1 || a > 10) {
+            if ((a < 1 || a > 10) || (b < 1 || b > 10)) {
                 throw new Exception("Числа должны быть от 1 до 10 включительно");
             }
-            if (b < 1 || b > 10) {
-                throw new Exception("Числа должны быть от 1 до 10 включительно");
+            switch (action) {
+                case '+':
+                    int sum = a + b;
+                    result = sum + "";
+                    break;
+                case '-':
+                    int s = a - b;
+                    result = s + "";
+                    break;
+                case '/':
+                    int h = a / b;
+                    result = h + "";
+                    break;
+                case '*':
+                    int multi = a * b;
+                    result = multi + "";
+                    break;
+                default:
+                    System.out.println("Нет такой операции!");
+
             }
-            double d = a + b;
-            int c = (int) d;
-            result = c + "";
-        } else if (input.contains("-")) {
-            String[] split = input.split("[-]");
-            if (split.length > 2) {
-                throw new Exception("формат математической операции не удовлетворяет заданию" +
-                        " - два операнда и один оператор (+, -, /, *)");
-            }
-            double a = Double.parseDouble(split[0]);
-            double b = Double.parseDouble(split[1]);
-            if (a % 1 != 0) {
-                throw new Exception("Калькулятор работает только с целыми числами");
-            }
-            if (b % 1 != 0) {
-                throw new Exception("Калькулятор работает только с целыми числами");
-            }
-            if (a < 1 || a > 10) {
-                throw new Exception("Числа должны быть от 1 до 10 включительно");
-            }
-            if (b < 1 || b > 10) {
-                throw new Exception("Числа должны быть от 1 до 10 включительно");
-            }
-            double d = a - b;
-            int c = (int) d;
-            result = c + "";
-        } else if (input.contains("*")) {
-            String[] split = input.split("[*]");
-            if (split.length > 2) {
-                throw new Exception("формат математической операции не удовлетворяет заданию" +
-                        " - два операнда и один оператор (+, -, /, *)");
-            }
-            double a = Double.parseDouble(split[0]);
-            double b = Double.parseDouble(split[1]);
-            if (a % 1 != 0) {
-                throw new Exception("Калькулятор работает только с целыми числами");
-            }
-            if (b % 1 != 0) {
-                throw new Exception("Калькулятор работает только с целыми числами");
-            }
-            if (a < 1 || a > 10) {
-                throw new Exception("Числа должны быть от 1 до 10 включительно");
-            }
-            if (b < 1 || b > 10) {
-                throw new Exception("Числа должны быть от 1 до 10 включительно");
-            }
-            double d = a * b;
-            int c = (int) d;
-            result = c + "";
-        } else if (input.contains("/")) {
-            String[] split = input.split("[/]");
-            if (split.length > 2) {
-                throw new Exception("формат математической операции не удовлетворяет заданию" +
-                        " - два операнда и один оператор (+, -, /, *)");
-            }
-            double a = Double.parseDouble(split[0]);
-            double b = Double.parseDouble(split[1]);
-            if (a % 1 != 0) {
-                throw new Exception("Калькулятор работает только с целыми числами");
-            }
-            if (b % 1 != 0) {
-                throw new Exception("Калькулятор работает только с целыми числами");
-            }
-            if (a < 1 || a > 10) {
-                throw new Exception("Числа должны быть от 1 до 10 включительно");
-            }
-            if (b < 1 || b > 10) {
-                throw new Exception("Числа должны быть от 1 до 10 включительно");
-            }
-            double d = a / b;
-            int c = (int) d;
-            result = c + "";
+
         } else throw new Exception("Строка не является математической операцией");
         return result;
     }
-
-
 }
-
